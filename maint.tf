e2a02bd78365580065e23e7bfc178eaa7dc75864
a-yossy <atsuya0930@yahoo.co.jp>
2025-02-19 01:23:55 +0900

    chapter6

--- maint.tf
+++ maint.tf
@@ -36,11 +36,22 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1c"
 
   tags = {
     "Name" = "public"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-northeast-1c"
+
+  tags = {
+    "Name" = "private"
   }
 }
 
@@ -86,7 +97,7 @@ resource "aws_security_group" "web" {
   description = "web security group"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ssh" {
+resource "aws_vpc_security_group_ingress_rule" "web_ssh" {
   security_group_id = aws_security_group.web.id
   from_port         = 22
   to_port           = 22
@@ -94,7 +105,7 @@ resource "aws_vpc_security_group_ingress_rule" "ssh" {
   cidr_ipv4         = "0.0.0.0/0"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "http" {
+resource "aws_vpc_security_group_ingress_rule" "web_http" {
   security_group_id = aws_security_group.web.id
   from_port         = 80
   to_port           = 80
@@ -102,7 +113,15 @@ resource "aws_vpc_security_group_ingress_rule" "http" {
   cidr_ipv4         = "0.0.0.0/0"
 }
 
-resource "aws_vpc_security_group_egress_rule" "all" {
+resource "aws_vpc_security_group_ingress_rule" "web_icmp" {
+  security_group_id = aws_security_group.web.id
+  from_port         = -1
+  to_port           = -1
+  ip_protocol       = "icmp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_egress_rule" "web_all" {
   security_group_id = aws_security_group.web.id
   ip_protocol       = "-1"
   cidr_ipv4         = "0.0.0.0/0"
@@ -135,4 +154,59 @@ resource "aws_eip" "web" {
 resource "aws_eip_association" "web" {
   instance_id   = aws_instance.web.id
   allocation_id = aws_eip.web.id
+}
+
+resource "aws_security_group" "db" {
+  vpc_id      = aws_vpc.main.id
+  name        = "db"
+  description = "db security group"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "db_ssh" {
+  security_group_id = aws_security_group.db.id
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "db_mysql" {
+  security_group_id = aws_security_group.db.id
+  from_port         = 3306
+  to_port           = 3306
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "db_icmp" {
+  security_group_id = aws_security_group.db.id
+  ip_protocol       = "icmp"
+  from_port         = -1
+  to_port           = -1
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_egress_rule" "db_all" {
+  security_group_id = aws_security_group.db.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_instance" "db" {
+  ami                    = data.aws_ami.amazon_linux_2023.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.private.id
+  vpc_security_group_ids = [aws_security_group.db.id]
+  private_ip             = "10.0.2.10"
+  key_name               = "web"
+  ebs_optimized          = true
+
+  ebs_block_device {
+    volume_size = 8
+    device_name = "/dev/xvda"
+  }
+
+  tags = {
+    "Name" = "db"
+  }
 }
